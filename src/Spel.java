58e4b3053344ec3f86bc605687b1c7538908e537
MarkusRissanenWennerkvist <markus.wennerkvist@yh.nackademin.se>
2025-11-04 11:18:41 +0100

    Ändrade spel så att den kan ta emot slumparen / randomizern när jag mergar dem senare.

--- src/Spel.java
+++ src/Spel.java
@@ -4,22 +4,26 @@ import javax.swing.*;
 
 public class Spel extends JFrame {
 
-    JPanel spelbräde = new JPanel();
-    JButton[] knappLista = new JButton[16];
+    private JPanel spelbräde = new JPanel();
+    private JButton[] knappLista = new JButton[16];
+
     public Spel(){
         this.add(spelbräde);
         spelbräde.setLayout(new GridLayout(4,4));
         for (int i = 0; i < 16; i++){
             JButton knapp = new JButton();
             knappLista[i] = knapp;
-            for (JButton bricka : knappLista){
-                spelbräde.add(knapp);
-            }
+            spelbräde.add(knapp);
         }
+        Randomizer random = new Randomizer(this);
+        random.slumpabräde();
         setSize(300,400);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    public JButton[] getKnappLista() {
+        return knappLista;
+    }
 }
