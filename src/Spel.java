00eb7871b0a009ea7cc7573f4c628ef058752128
MarkusRissanenWennerkvist <markus.wennerkvist@yh.nackademin.se>
2025-11-04 09:21:37 +0100

    Jag skapade den absoluta grunden för gui dvs en panel med 16 tomma knappar med allt som behövs för att de ska synas på skärmen.

--- src/Spel.java
+++ src/Spel.java
@@ -4,4 +4,22 @@ import javax.swing.*;
 
 public class Spel extends JFrame {
 
+    JPanel spelbräde = new JPanel();
+    JButton[] knappLista = new JButton[16];
+    public Spel(){
+        this.add(spelbräde);
+        spelbräde.setLayout(new GridLayout(4,4));
+        for (int i = 0; i < 16; i++){
+            JButton knapp = new JButton();
+            knappLista[i] = knapp;
+            for (JButton bricka : knappLista){
+                spelbräde.add(knapp);
+            }
+        }
+        setSize(300,400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
 }
