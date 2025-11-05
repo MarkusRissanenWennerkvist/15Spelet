024e4d98b93f08a261d54592b0b1277e522e5fb9
MarkusRissanenWennerkvist <markus.wennerkvist@yh.nackademin.se>
2025-11-05 09:11:02 +0100

    nu har jag skapat en till metod i randomizer klassen som skaar en semi vinst dvs alla utom en är korrekt och det gäller bara att flytta på en knapp för att vinna ska snart implmenetera knappar för nytt spel och semi vinst :)

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -27,5 +27,18 @@ public class Randomizer {
     //lista ut hur jag gör hela brädet men två rutor är fel
     // for loop 1-14 sätt den toma knappen på index 14
 
+    public void ordnabräde(){
+        ArrayList<String> siffror = new ArrayList<>();
+        for (int i = 0; i <= 14; i++){
+            knappar[i].setText(String.valueOf(i + 1));
+        }
+
+        for (int i = 0; i < 15; i++){
+            knappar[i].setText(siffror.get(i));
+        }
+        knappar[14].setText("");
+        knappar[15].setText("15");
+    }
+
 
 }
